921a16f937bbf324123e8c5e523117d06dbd04aa
jjstn-lee <justinlee5483@gmail.com>
2024-02-07 21:04:54 -0500

    Initial Commit

---
+++ progressive-overload/progressive_overloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  progressive_overloadApp.swift
+//  progressive-overload
+//
+//  Created by justin lee on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct progressive_overloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
