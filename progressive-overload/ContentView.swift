bbd0a19ba2a39cab87c1c88ab11201207f9383e1
jjstn-lee <justinlee5483@gmail.com>
2024-02-12 17:04:14 -0500

    abstracted number box out

--- progressive-overload/ContentView.swift
+++ progressive-overload/ContentView.swift
@@ -6,25 +6,61 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Spacer()
-            HStack(alignment: .top) {
-                NavigationLink("new workout", destination: NewWorkout())
-                
-            }
+            WorkoutPanel()
         }
         .padding()
     }
 }
 
-struct NewWorkout: View {
+struct WorkoutPanel: View {
     var body: some View {
-        Text("new workout page");
+        HStack {
+            WorkoutRow(setID: 4);
+        }
     }
 }
+
+struct WorkoutRow: View {
+    @State private var setID: Int;
+    init(setID: Int) {
+        self.setID = setID;
+    }
+    
+    var body: some View {
+        HStack {
+            Text("\(setID)");
+            NumberBox(isReps: true);
+            NumberBox(isReps: false);
+        }
+    }
+}
+
+struct NumberBox: View {
+    @State private var numericInput: String = "";
+    @State private var isReps: Bool;
+    init(isReps: Bool) {
+            self.isReps = isReps
+        }
+    var body: some View {
+        TextField(isReps ? "reps" : "weight", text: $numericInput)
+            .keyboardType(.numberPad)
+            .multilineTextAlignment(.trailing)
+            .frame(width: 50)
+        // only allows decimal numbers
+            .onReceive(Just(numericInput)) { newNumericInput in
+                let filtered = newNumericInput.filter {"0123456789".contains($0) }
+                if filtered != newNumericInput {
+                    self.numericInput = filtered
+                }
+            }
+    }
+}
+
 
 
 //struct NavigationBar: View {
